7e33c0de6e261d22f09a84a850d64a765cdc8cfa
Hikmahx <hikmayousuph@gmail.com>
2025-10-29 21:27:56 +0100

    colors moved into extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,12 @@ module.exports = {
       fontSize: {
         base: '15px',
       },
-    },
-    colors: {
-      'cyan-400': 'rgb(91, 164, 164)',
-      'cyan-50': 'rgb(239, 250, 250)',
-      'grayish-cyan': 'rgb(123, 142, 142)',
-      'cyan-900': 'rgb(44, 58, 58)',
+      colors: {
+        'cyan-400': 'rgb(91, 164, 164)',
+        'cyan-50': 'rgb(239, 250, 250)',
+        'grayish-cyan': 'rgb(123, 142, 142)',
+        'cyan-900': 'rgb(44, 58, 58)',
+      },
     },
     fontFamily: {
       spartan: ['Spartan', 'sans-serif'],
