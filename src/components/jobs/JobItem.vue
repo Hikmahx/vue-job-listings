544c182ac129b84c99d92f134bd57ba1c538e92a
Hikmahx <hikmayousuph@gmail.com>
2025-10-30 00:18:56 +0100

    Job type transferred to types folder

--- src/components/jobs/JobItem.vue
+++ src/components/jobs/JobItem.vue
@@ -1,20 +1,5 @@
 <script setup lang="ts">
-
-interface Job {
-  id: number
-  company: string
-  logo: string
-  new: boolean
-  featured: boolean
-  position: string
-  role: string
-  level: string
-  postedAt: string
-  contract: string
-  location: string
-  languages: string[]
-  tools: string[]
-}
+import type { Job } from '@/types'
 
 const { job } = defineProps<{ job: Job }>()
 </script>
