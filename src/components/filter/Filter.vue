bded799cde619bb4dac33d653abcc9c55e6f999d
Hikmahx <hikmayousuph@gmail.com>
2025-10-30 20:49:48 +0100

    Filter and FilterBtn components

---
+++ src/components/filter/Filter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import FilterBtn from './FilterBtn.vue'
+
+interface FilterProps {
+  selectedBtn: string[]
+  removeBtn: (btn: string) => void
+  clearAllBtns: () => void
+}
+
+defineProps<FilterProps>()
+</script>
+
+<template>
+  <div class="px-10">
+    <div
+      :class="`${
+        selectedBtn.length > 0 ? 'block' : 'hidden'
+      } w-full max-w-3xl lg:max-w-7xl mx-auto bg-white px-6 py-4 mb-16 relative h-auto rounded-md shadow-[0_16px_26px_-6px_#d7e9ec] -top-10`"
+    >
+      <div class="flex flex-wrap gap-4">
+        <FilterBtn :btns="selectedBtn" :removeBtn="removeBtn" />
+      </div>
+      <button
+        class="absolute right-6 top-4 text-teal-600 font-semibold cursor-pointer hover:underline"
+        @click="clearAllBtns"
+      >
+        Clear
+      </button>
+    </div>
+  </div>
+</template>
---
+++ src/components/filter/FilterBtn.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import removeIcon from '@/assets/img/icon-remove.svg'
+
+interface FilterBtnProps {
+  btns: string[]
+  removeBtn: (btn: string) => void
+}
+
+defineProps<FilterBtnProps>()
+</script>
+
+<template>
+  <div v-for="(btn, index) in btns" :key="index">
+    <button
+      class="flex items-center gap-2 bg-cyan-50 text-cyan-400 font-semibold rounded-md overflow-hidden"
+    >
+      <span class="px-2">{{ btn }}</span>
+      <span
+        class="bg-teal-600 hover:bg-cyan-400 p-2 cursor-pointer flex items-center justify-center"
+        @click="() => removeBtn(btn)"
+      >
+        <img :src="removeIcon" alt="Remove" class="w-3 h-3" />
+      </span>
+    </button>
+  </div>
+</template>
