74b10c634e2c15af16a8035adbf782a1b0429ac0
Hikmahx <hikmayousuph@gmail.com>
2025-11-01 12:19:06 +0100

    functionalities and styles updated

--- src/components/jobs/JobItem.vue
+++ src/components/jobs/JobItem.vue
@@ -1,70 +1,91 @@
 <script setup lang="ts">
 import type { Job } from '@/types'
 
-const { job } = defineProps<{ job: Job }>()
+defineProps<{ job: Job; onClickFilter: (event: Event) => void }>()
 </script>
 
 <template>
   <li
-    :class="`relative bg-white rounded-md shadow-[0_12px_16px_0_#d7e9ec] h-[25.7rem] mb-16 p-7 px-9 ${job.featured && `before:content-[''] before:absolute before:w-2 before:h-full before:bg-[#5ea4a6] before:top-0 before:left-0`} before:rounded-l-md`"
+    :class="`relative flex flex-col lg:flex-row gap-12 lg:gap-4 bg-white rounded-md shadow-[0_12px_16px_0_#d7e9ec] mb-16 lg:mb-10 p-7 px-6 lg:px-10 ${job.featured && `before:content-[''] before:absolute before:w-1.5 before:h-full before:bg-[#5ea4a6] before:top-0 before:left-0`} before:rounded-l-md`"
   >
-    <img :src="`${job.logo}`" alt="banner" class="relative w-12 h-12 -top-16 mb-[-16px]" />
+    <img
+      :src="`${job.logo}`"
+      alt="banner"
+      class="relative w-16 h-16 lg:w-[88px] lg:h-[88px] -top-16 lg:top-0 mb-[-16px]"
+    />
+    <div class="flex-1 flex flex-col lg:flex-row lg:items-center">
+      <div class="">
+        <div class="flex items-center justify-start gap-1 flex-wrap">
+          <h2 class="text-sm text-cyan-400 mr-5 whitespace-nowrap font-semibold">
+            {{ job.company }}
+          </h2>
+          <span
+            v-if="job.new"
+            class="bg-cyan-400 text-white uppercase px-2 pt-1 h-6 rounded-full flex items-center justify-center text-[11px] font-bold"
+          >
+            New!
+          </span>
+          <span
+            v-if="job.featured"
+            class="bg-black text-white uppercase px-2 pt-1 h-6 rounded-full flex items-center justify-center text-[11px] font-bold"
+          >
+            Featured
+          </span>
+        </div>
 
-    <div class="flex items-center justify-start gap-1">
-      <h2 class="text-sm text-cyan-400 mr-5 whitespace-nowrap font-semibold">{{ job.company }}</h2>
-      <span
-        v-if="job.new"
-        class="bg-cyan-400 text-white uppercase px-3 pt-1 rounded-full flex items-center justify-center text-xs font-bold"
-      >
-        New!
-      </span>
-      <span
-        v-if="job.featured"
-        class="bg-black text-white uppercase px-3 py-1 rounded-full flex items-center justify-center text-xs font-bold"
-      >
-        Feature
-      </span>
-    </div>
+        <h3
+          class="mt-3 hover:text-cyan-400 transition-colors duration-200 cursor-pointer text-base font-bold"
+        >
+          {{ job.position }}
+        </h3>
 
-    <h3
-      class="mt-3 hover:text-cyan-400 transition-colors duration-200 cursor-pointer text-base font-bold"
-    >
-      {{ job.position }}
-    </h3>
+        <div class="mt-4 flex items-center flex-wrap text-sm text-[#7b8e8e]">
+          <span
+            class="relative mr-6 after:content-['.'] after:ml-1 after:text-3xl after:absolute after:top-[-1rem] after:opacity-70 after:blur-[0.06rem]"
+          >
+            {{ job.postedAt }}
+          </span>
+          <span
+            class="relative mr-6 after:content-['.'] after:ml-1 after:text-3xl after:absolute after:top-[-1rem] after:opacity-70 after:blur-[0.06rem]"
+          >
+            {{ job.contract }}
+          </span>
+          <span>{{ job.location }}</span>
+        </div>
+      </div>
 
-    <div
-      class="mt-4 pb-6 border-b border-[#c0c2c1] flex items-center flex-wrap text-sm text-[#7b8e8e]"
-    >
-      <span
-        class="relative mr-6 after:content-['.'] after:ml-1 after:text-3xl after:absolute after:top-[-1rem] after:opacity-70 after:blur-[0.06rem]"
-      >
-        {{ job.postedAt }}
-      </span>
-      <span
-        class="relative mr-6 after:content-['.'] after:ml-1 after:text-3xl after:absolute after:top-[-1rem] after:opacity-70 after:blur-[0.06rem]"
-      >
-        {{ job.contract }}
-      </span>
-      <span>{{ job.location }}</span>
-    </div>
-
-    <div class="mt-4 flex flex-wrap">
-      <button
-        v-for="role in job.languages"
-        :data-role="role"
-        :key="role"
-        class="mb-4 mr-3 px-3 py-2 bg-[#eef6f6] text-cyan-400 font-bold rounded-sm hover:bg-cyan-400 hover:text-white transition-colors duration-200"
-      >
-        {{ role }}
-      </button>
-      <button
-        v-for="tool in job.tools"
-        :data-tool="tool"
-        :key="tool"
-        class="mb-4 mr-3 px-3 py-2 bg-[#eef6f6] text-cyan-400 font-bold rounded-sm hover:bg-cyan-400 hover:text-white transition-colors duration-200"
-      >
-        {{ tool }}
-      </button>
+      <div class="mt-4 flex flex-wrap gap-x-3 gap-y-4 lg:ml-auto">
+        <button
+          @click="onClickFilter"
+          class="px-3 py-2 h-8 bg-[#eef6f6] text-cyan-400 font-bold rounded-sm hover:bg-cyan-400 hover:text-white transition-colors duration-200"
+        >
+          {{ job.role }}
+        </button>
+        <button
+          @click="onClickFilter"
+          class="px-3 py-2 h-8 bg-[#eef6f6] text-cyan-400 font-bold rounded-sm hover:bg-cyan-400 hover:text-white transition-colors duration-200"
+        >
+          {{ job.level }}
+        </button>
+        <button
+          v-for="language in job.languages"
+          :data-language="language"
+          :key="language"
+          @click="onClickFilter"
+          class="px-3 py-2 h-8 bg-[#eef6f6] text-cyan-400 font-bold rounded-sm hover:bg-cyan-400 hover:text-white transition-colors duration-200"
+        >
+          {{ language }}
+        </button>
+        <button
+          v-for="tool in job.tools"
+          :data-tool="tool"
+          :key="tool"
+          @click="onClickFilter"
+          class="px-3 py-2 h-8 bg-[#eef6f6] text-cyan-400 font-bold rounded-sm hover:bg-cyan-400 hover:text-white transition-colors duration-200"
+        >
+          {{ tool }}
+        </button>
+      </div>
     </div>
   </li>
 </template>
--- src/components/jobs/Jobs.vue
+++ src/components/jobs/Jobs.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
-import jobData from '@/data.json'
-import { ref } from 'vue'
+import { computed } from 'vue'
 import JobItem from './JobItem.vue'
+import type { Job } from '@/types'
 
-const jobs = ref(jobData)
+const { jobs, onClickFilter, selectedBtn } = defineProps<{
+  jobs: Job[]
+  onClickFilter: (event: Event) => void
+  selectedBtn: string[]
+}>()
 
-console.log(jobs.value)
+const filteredJobs = computed(() => {
+  if (selectedBtn.length === 0) {
+    return jobs
+  }
+
+  return jobs.filter((job) => {
+    const jobAttributes = [job.role, job.level, ...job.languages, ...job.tools]
+
+    return selectedBtn.every((selected) => jobAttributes.includes(selected))
+  })
+})
 </script>
 
 <template>
   <ul class="pt-24 px-10 pb-px">
-    <JobItem v-for="job in jobs" :key="job.id" :job="job" />
+    <JobItem v-for="job in filteredJobs" :key="job.id" :job="job" :onClickFilter="onClickFilter" />
   </ul>
 </template>
 
