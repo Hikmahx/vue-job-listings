489e97dafe191fa805baf5aa182cf8c0e066486b
Hikmahx <hikmayousuph@gmail.com>
2025-11-15 23:31:54 +0100

    work type added

--- client/src/components/filter/FilterModal.vue
+++ client/src/components/filter/FilterModal.vue
@@ -7,32 +7,54 @@ import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
+  // DialogDescription,
   DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+  // DialogHeader,
+  // DialogTitle,
+  // DialogTrigger,
 } from '@/components/ui/dialog'
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from '@/components/ui/form'
+// import { Input } from '@/components/ui/input'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { useForm, Field as VeeField } from 'vee-validate'
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-
+  Field,
+  FieldContent,
+  FieldDescription,
+  // FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+  FieldTitle,
+} from '@/components/ui/field'
 const formSchema = toTypedSchema(
   z.object({
+    workType: z.string().min(2).max(50),
     keyword: z.string().min(2).max(50),
+    // keyword: z.string().min(2).max(50),
+    // keyword: z.string().min(2).max(50),
   }),
 )
 
+const { handleSubmit, resetForm } = useForm({
+  validationSchema: formSchema,
+  initialValues: {
+    workType: '',
+  },
+})
+
 const showDialog = ref(false)
 
-function onSubmit(values: any) {
+const onSubmit = handleSubmit((values) => {
   console.log('Form submitted with values:', values)
   console.log({
     title: 'You submitted the following values:',
@@ -44,43 +66,66 @@ function onSubmit(values: any) {
   })
 
   showDialog.value = false
-}
+})
 </script>
 
 <template>
-  <Form v-slot="{ handleSubmit }" as="" keep-values :validation-schema="formSchema">
+  <form @submit="onSubmit">
     <Button variant="default" class="bg-cyan-400" @click="showDialog = true">
       Advanced Filters
     </Button>
-    
-    <Dialog :open="showDialog" @update:open="(value) => showDialog = value">
-      <DialogContent class="sm:max-w-[425px]">
-        <DialogHeader>
+
+    <Dialog :open="showDialog" @update:open="(value) => (showDialog = value)">
+      <DialogContent class="sm:max-w-2xl lg:max-w-5xl w-[95%] mx-auto py-12">
+        <!-- <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
-        </DialogHeader>
+        </DialogHeader> -->
+        <div class="flex flex-wrap gap-5">
+          <FieldGroup class="">
+            <VeeField v-slot="{ field, errors }" name="workType">
+              <FieldSet>
+                <FieldLegend class="font-bold mb-6">Work Type</FieldLegend>
+                <RadioGroup
+                  :name="field.name"
+                  :model-value="field.value"
+                  :aria-invalid="!!errors.length"
+                  @update:model-value="field.onChange"
+                  class="grid grid-cols-3 gap-3"
+                >
+                  <FieldLabel
+                    v-for="(workType, index) in ['remote', 'hybrid', 'onsite']"
+                    :key="index"
+                    :for="`form-vee-radiogroup-${workType}`"
+                    class="!border-2 has-[[data-state=checked]]:!bg-cyan-400/30 has-[[data-state=checked]]:border-2 has-[[data-state=checked]]:!border-cyan-900/50 has-[[data-state=checked]]:!text-cyan-900/90 has-[[data-state=checked]]:!font-bold capitalize"
+                  >
+                    <Field orientation="horizontal" class="flex-row-reverse">
+                      <FieldContent>
+                        <FieldTitle>{{ workType }}</FieldTitle>
+                      </FieldContent>
+                      <RadioGroupItem
+                        :id="`form-vee-radiogroup-${workType}`"
+                        :value="workType"
+                        :aria-invalid="!!errors.length"
+                        class="accent-cyan-400"
+                      />
+                    </Field>
+                  </FieldLabel>
+                </RadioGroup>
+                <!-- <FieldError v-if="errors.length"
+               :errors="errors"
+                /> -->
+              </FieldSet>
+            </VeeField>
+          </FieldGroup>
 
-        <form id="dialogForm" @submit="handleSubmit($event, onSubmit)">
-          <FormField v-slot="{ componentField }" name="keyword">
-            <FormItem>
-              <FormLabel>Keyword</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="shadcn" v-bind="componentField" />
-              </FormControl>
-              <FormDescription> This is your public display name. </FormDescription>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-        </form>
-
+        </div>
         <DialogFooter>
-          <Button type="submit" form="dialogForm">
-            Save changes
-          </Button>
+          <Button type="submit" form="dialogForm"> Save changes </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  </Form>
+  </form>
 </template>
