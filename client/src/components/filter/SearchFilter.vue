c75626ac4a1bb0bf36f468f1364d9dbef2e3ed1d
Hikmahx <hikmayousuph@gmail.com>
2025-11-19 19:24:34 +0100

    form submit implemented

--- client/src/components/filter/SearchFilter.vue
+++ client/src/components/filter/SearchFilter.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue'
 // import FilterBtn from './FilterBtn.vue'
 import FilterModal from './FilterModal.vue'
 import SearchAndCountry from './SearchAndCountry.vue'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+import { useForm } from 'vee-validate'
+import { FieldGroup, FieldLabel } from '@/components/ui/field'
 
 interface SearchFormProps {
   selectedBtn: string[]
@@ -12,27 +16,38 @@ interface SearchFormProps {
 
 defineProps<SearchFormProps>()
 
-const showModal = ref(false)
 const sortByDate = ref(false)
-
-const toggleModal = () => {
-  showModal.value = !showModal.value
-}
 
 const handleSearch = () => {
   console.log('Searching...', { sortByDate: sortByDate.value })
-  showModal.value = false
 }
 
+const formSchema = toTypedSchema(
+  z.object({
+    search: z.string().optional(),
+    country: z.string().optional(),
+  })
+)
 
+const { handleSubmit } = useForm({
+  validationSchema: formSchema,
+  initialValues: {
+    search: '',
+    country: '',
+  },
+})
+
+const handleFormSubmit = handleSubmit((values) => {
+  console.log('Form submitted on change:', values)
+})
 </script>
 
 <template>
   <div class="px-10 w-full max-w-3xl lg:max-w-6xl m-auto">
     <div class="bg-white rounded-lg shadow-lg p-8 -mt-12 lg:-mt-20 relative z-20">
-      <div class="mb-6">
+      <form @change="handleFormSubmit" class="mb-6">
         <SearchAndCountry />
-      </div>
+      </form>
 
       <!-- Below the filter tags-->
       <div class="flex items-center justify-between pt-6 mt-6 border-t">
@@ -47,7 +62,7 @@ const handleSearch = () => {
             <span class="text-sm"> Sort by Date </span>
           </label>
         </div>
-          <FilterModal />
+        <FilterModal />
       </div>
     </div>
   </div>
