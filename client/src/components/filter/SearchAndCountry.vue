32c4fde61ca4111b58a9724ec98b4b0326a2dd5f
Hikmahx <hikmayousuph@gmail.com>
2025-11-19 19:26:28 +0100

    component modified and fixed

--- client/src/components/filter/SearchAndCountry.vue
+++ client/src/components/filter/SearchAndCountry.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-// import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-// import { Check, MapPin, Search } from 'lucide-vue-next'
+import { MapPin, Search } from 'lucide-vue-next'
 import {
   Combobox,
   ComboboxAnchor,
@@ -9,62 +8,69 @@ import {
   ComboboxGroup,
   ComboboxInput,
   ComboboxItem,
-  ComboboxItemIndicator,
   ComboboxList,
 } from '@/components/ui/combobox'
-import { reactive } from 'vue'
-// defineProps<{
-//   state: {
-//     countries: { value: string; label: string }[]
-//   }
-// }>()
+import { Field, FieldLabel } from '@/components/ui/field'
+import { Field as VeeField } from 'vee-validate'
 
-const state = reactive({
-  countries: [
-    { value: 'worldwide', label: 'Worldwide' },
-    { value: 'usa', label: 'United States' },
-    { value: 'uk', label: 'United Kingdom' },
-    { value: 'canada', label: 'Canada' },
-    { value: 'australia', label: 'Australia' },
-    { value: 'germany', label: 'Germany' },
-    { value: 'france', label: 'France' },
-    { value: 'spain', label: 'Spain' },
-    { value: 'nigeria', label: 'Nigeria' },
-    { value: 'italy', label: 'Italy' },
-    { value: 'netherlands', label: 'Netherlands' },
-    { value: 'india', label: 'India' },
-    { value: 'singapore', label: 'Singapore' },
-    { value: 'japan', label: 'Japan' },
-    { value: 'brazil', label: 'Brazil' },
-    { value: 'mexico', label: 'Mexico' },
-  ],
-})
-
+const countries = [
+  { value: 'worldwide', label: 'Worldwide' },
+  { value: 'usa', label: 'United States' },
+  { value: 'uk', label: 'United Kingdom' },
+  { value: 'canada', label: 'Canada' },
+  { value: 'australia', label: 'Australia' },
+  { value: 'germany', label: 'Germany' },
+  { value: 'france', label: 'France' },
+  { value: 'spain', label: 'Spain' },
+  { value: 'nigeria', label: 'Nigeria' },
+  { value: 'italy', label: 'Italy' },
+  { value: 'netherlands', label: 'Netherlands' },
+  { value: 'india', label: 'India' },
+  { value: 'singapore', label: 'Singapore' },
+  { value: 'japan', label: 'Japan' },
+  { value: 'brazil', label: 'Brazil' },
+  { value: 'mexico', label: 'Mexico' },
+]
 </script>
+
 <template>
   <div class="flex gap-3 w-full">
-    <!-- {/* Search Input */} -->
-    <div class="relative flex-1">
-      <span class="absolute start-0 inset-y-0 flex items-center justify-center px-3">
-        <Search class="size-4 text-muted-foreground" />
-      </span>
-      <Input
-        type="text"
-        placeholder="Search by role, skill, or company..."
-        class="w-full pl-9 h-12 pr-4 py-4 transition-all focus:outline-none focus:ring-2"
-      />
+    <!-- Search Input -->
+    <div class="flex-1">
+      <VeeField v-slot="{ field, errors }" name="search">
+        <Field :data-invalid="!!errors.length" class="relative">
+          <FieldLabel class="sr-only">Search Jobs</FieldLabel>
+          <span class="absolute left-0 inset-y-0 flex items-center px-3 -z-10 w-4">
+            <Search class="size-4 text-muted-foreground" />
+          </span>
+          <Input
+            type="text"
+            placeholder="Search by role, skill, or company..."
+            class="w-full pl-9 h-12 pr-4 py-4 transition-all focus:outline-none focus:ring-2"
+            :id="`form-vee-search`"
+            v-bind="field"
+            :model-value="field.value"
+            :aria-invalid="!!errors.length"
+            @update:model-value="field.onChange"
+          />
+        </Field>
+      </VeeField>
     </div>
 
+    <!-- Country Select -->
     <div class="relative">
-      <div
-        class="flex items-center gap-2 transition-all hover:shadow-md whitespace-nowrap min-w-[200px] justify-between"
-      >
-        <Combobox v-model="state.countries" by="countries">
+      <VeeField v-slot="{ field }" name="country">
+        <Combobox :model-value="field.value" @update:model-value="field.onChange">
           <ComboboxAnchor>
             <div class="relative w-full max-w-sm items-center">
               <ComboboxInput
                 class="pl-9 h-12"
-                :display-value="(val) => val?.label ?? ''"
+                :display-value="
+                  (val) => {
+                    const country = countries.find((c) => c.value === val)
+                    return country?.label || ''
+                  }
+                "
                 placeholder="Select country..."
               />
               <span class="absolute start-0 inset-y-0 flex items-center justify-center px-3">
@@ -73,19 +79,19 @@ const state = reactive({
             </div>
           </ComboboxAnchor>
           <ComboboxList>
-            <ComboboxEmpty> No country found. </ComboboxEmpty>
+            <ComboboxEmpty>No country found.</ComboboxEmpty>
             <ComboboxGroup>
               <ComboboxItem
-                v-for="country in state.countries"
+                v-for="country in countries"
                 :key="country.value"
-                :value="country"
+                :value="country.value"
               >
                 {{ country.label }}
               </ComboboxItem>
             </ComboboxGroup>
           </ComboboxList>
         </Combobox>
-      </div>
+      </VeeField>
     </div>
   </div>
 </template>
