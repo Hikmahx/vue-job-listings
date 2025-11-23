23bb86d222169f31c8461a2f8984812111042d5c
Hikmahx <hikmayousuph@gmail.com>
2025-11-23 19:44:43 +0100

    fomatSalary, selectedBtns, removeBtn, and clearAllBtns functions added to filter component

--- client/src/components/filter/Filter.vue
+++ client/src/components/filter/Filter.vue
@@ -1,25 +1,128 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { inject, computed } from 'vue'
 import FilterBtn from './FilterBtn.vue'
+import { spokenLanguages } from '@/constants/filters'
 
-interface FilterProps {
-  selectedBtn: string[]
-  removeBtn: (btn: string) => void
-  clearAllBtns: () => void
+const filters = inject('filters')
+const groupedFilters = inject('groupedFilters')
+
+const formatSalary = (salary: number) => {
+  if (salary >= 1000000) return `${(salary / 1000000).toFixed(0)}M`
+  if (salary >= 1000) return `${(salary / 1000).toFixed(0)}k`
+  return salary.toString()
 }
 
-defineProps<FilterProps>()
+const selectedBtns = computed(() => {
+  const filterData = groupedFilters(filters.value)
+  const btns = []
+
+  for (const item of filterData) {
+    const [key, value] = Object.entries(item)[0]
+
+    if (key === 'search' || key === 'country' || key === 'sortByDate') continue
+
+    switch (key) {
+      case 'workType':
+        btns.push({
+          text: value.charAt(0).toUpperCase() + value.slice(1),
+          key: 'workType',
+          value: '',
+        })
+        break
+
+      case 'spokenLanguages':
+        const lang = spokenLanguages.find((opt) => opt.value === value)
+        btns.push({
+          text: lang?.label || value,
+          key: 'spokenLanguages',
+          value: '',
+        })
+        break
+
+      case 'minSalary':
+      case 'maxSalary':
+        const min = filterData.find((item) => item.minSalary)?.['minSalary']
+        const max = filterData.find((item) => item.maxSalary)?.['maxSalary']
+        const currency = filterData.find((item) => item.currency)?.['currency'] || ''
+
+        let salaryText = ''
+        if (min && max)
+          salaryText = `${currency}${formatSalary(min)}-${currency}${formatSalary(max)}`
+        else if (min) salaryText = `>${currency}${formatSalary(min)}`
+        else if (max) salaryText = `<${currency}${formatSalary(max)}`
+
+        if (salaryText && !btns.some((btn) => btn.key === 'salary')) {
+          btns.push({
+            text: salaryText,
+            key: 'salary',
+            value: { minSalary: undefined, maxSalary: undefined, currency: '' },
+          })
+        }
+        break
+
+      case 'skills':
+      case 'markets':
+      case 'companySizes':
+      case 'jobTypes':
+      case 'roleTypes':
+        if (typeof value === 'number') {
+          btns.push({
+            text: value === 1 ? filters.value[key][0] : `${key} • ${value}`,
+            key: key,
+            value: [],
+          })
+        } else {
+          btns.push({
+            text: value,
+            key: key,
+            value: [],
+          })
+        }
+        break
+    }
+  }
+
+  return btns
+})
+
+const removeBtn = (btnText) => {
+  const btnToRemove = selectedBtns.value.find((btn) => btn.text === btnText)
+
+  if (btnToRemove) {
+    if (btnToRemove.key === 'salary') {
+      filters.value.minSalary = undefined
+      filters.value.maxSalary = undefined
+      filters.value.currency = ''
+    } else {
+      filters.value[btnToRemove.key] = btnToRemove.value
+    }
+    groupedFilters(filters.value)
+  }
+}
+
+const clearAllBtns = () => {
+  Object.assign(filters.value, {
+    minSalary: undefined,
+    maxSalary: undefined,
+    workType: '',
+    spokenLanguages: '',
+    skills: [],
+    markets: [],
+    companySizes: [],
+    jobTypes: [],
+    roleTypes: [],
+    currency: '',
+    sortByDate: false,
+  })
+  groupedFilters(filters.value)
+}
 </script>
 
 <template>
-  <div class="px-4 lg:px-10">
-    <div
-      :class="`${
-        selectedBtn.length > 0 ? 'block' : 'hidden'
-      } w-full max-w-3xl lg:max-w-6xl mx-auto bg-white px-6 py-4 mb-16 relative h-auto rounded-md shadow-[0_16px_26px_-6px_#d7e9ec] -top-10`"
-    >
+  <div :class="`${selectedBtns.length > 0 ? 'block' : 'hidden'} border-t pt-5`">
+    <div class="w-full relative h-auto">
       <div class="flex flex-wrap gap-4">
-        <FilterBtn :btns="selectedBtn" :removeBtn="removeBtn" />
+        <FilterBtn :btns="selectedBtns.map((btn) => btn.text)" :removeBtn="removeBtn" />
       </div>
       <button
         class="absolute right-6 top-4 text-teal-600 font-semibold cursor-pointer hover:underline"
