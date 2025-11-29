f3f93c4e7f2b5e85d5686551c52477afbd4035b4
Hikmahx <hikmayousuph@gmail.com>
2025-11-29 16:48:22 +0100

    url updated

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jobs/', include('jobs.api.urls'))
+    path('api/jobs/', include('jobs.api.urls'))
 ]
