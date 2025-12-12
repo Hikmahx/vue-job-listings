30882693435f30ef45d95b77c21c0a87b05e7e03
Hikmahx <hikmayousuph@gmail.com>
2025-12-12 19:06:35 +0100

    svg component

---
+++ client/src/components/icons/SvgSlanted.vue
@@ -0,0 +1,31 @@
+<script></script>
+
+<template>
+  <svg
+    width="1323"
+    height="862"
+    viewBox="0 0 1323 862"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      x="677.182"
+      y="16.8398"
+      width="291.94"
+      height="925.929"
+      rx="145.97"
+      transform="rotate(47 677.182 16.8398)"
+      fill="currentColor"
+      fill-opacity="0.4"
+    />
+    <rect
+      x="1123.18"
+      width="291.94"
+      height="925.929"
+      rx="145.97"
+      transform="rotate(47 1123.18 0)"
+      fill="currentColor"
+      fill-opacity="0.4"
+    />
+  </svg>
+</template>
