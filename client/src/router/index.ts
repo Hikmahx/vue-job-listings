c97ad1ca482731df49f49338ccfb33fe7731edf9
Hikmahx <hikmayousuph@gmail.com>
2025-12-21 21:21:49 +0100

    routterr

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,10 +1,12 @@
-import { createWebHistory, createRouter } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import JobsView from '@/views/JobsView.vue'
+import { createWebHistory, createRouter } from "vue-router"
+import HomeView from "@/views/HomeView.vue"
+import JobsView from "@/views/JobsView.vue"
+import JobDetailView from "@/views/JobDetailView.vue"
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/jobs', component: JobsView },
+  { path: "/", component: HomeView },
+  { path: "/jobs", component: JobsView },
+  { path: "/jobs/:id", component: JobDetailView, name: "job-detail" },
 ]
 
 export const router = createRouter({
