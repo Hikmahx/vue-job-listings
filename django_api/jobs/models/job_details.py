9ba47ff0b7f59a01e3bad0136bf3c2988372eedb
Hikmahx <hikmayousuph@gmail.com>
2025-12-25 23:11:17 +0100

    requirements and responsibilites changed to dict from lamba default to avoid error

--- django_api/jobs/models/job_details.py
+++ django_api/jobs/models/job_details.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class JobDetails(models.Model):
     description = models.TextField()
-    requirements = models.JSONField(default=lambda: {"content": "", "items": []})
-    responsibilities = models.JSONField(default=lambda: {"content": "", "items": []})
+    requirements = models.JSONField(default=dict)
+    responsibilities = models.JSONField(default=dict)
     external_apply = models.BooleanField(default=False)
     apply = models.URLField(blank=True, null=True)
     experience_required = models.CharField(max_length=100, blank=True, null=True)
