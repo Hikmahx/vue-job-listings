01907f22b5b33ba2a8ca0530ebb331b6c112fd03
Hikmahx <hikmayousuph@gmail.com>
2026-01-01 15:41:59 +0100

    backend routes

--- django_api/jobs/api/urls.py
+++ django_api/jobs/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from jobs.api.views import (GetAllJobsAPI, GetJobWithDetails)
+from .views import (
+    GetAllJobsAPI,
+    GetJobWithDetails,
+    CreateJobAPI,
+    UpdateJobAPI,
+    DeleteJobAPI, 
+)
 
 
 urlpatterns = [
+    path('create/', CreateJobAPI.as_view(), name='create-job'),
     path("", GetAllJobsAPI.as_view(), name='get-all-jobs'), 
     path("<str:id>/", GetJobWithDetails.as_view(), name='get-job-with-details'), 
+    path('update/<str:id>/', UpdateJobAPI.as_view(), name='update-job'),
+    path('delete/<str:id>/', DeleteJobAPI.as_view(), name='delete-job'),
 ]
