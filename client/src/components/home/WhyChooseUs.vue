6d1a9588571e3ad7cd145959e99198e0fcad5bd7
Hikmahx <hikmayousuph@gmail.com>
2026-01-05 19:52:35 +0100

    ai-powered search section added to why choose us section

--- client/src/components/home/WhyChooseUs.vue
+++ client/src/components/home/WhyChooseUs.vue
@@ -69,5 +69,34 @@ import SectionTitle from './SectionTitle.vue'
         </div>
       </div>
     </div>
+
+    <!-- Global Opportunities Section -->
+    <div class="grid md:grid-cols-2 gap-4 sm:gap-16 md:gap-3 lg:gap-10 items-center mt-12 lg:m-0 mb-12 lg:mb-16">
+      <div
+        class="order-2 md:order-1 max-w-xl m-auto text-center md:text-left md:ml-auto md:mr-0 px-4"
+      >
+        <h2
+          class="text-2xl font-semibold text-cyan-900 mb-4 tracking-[0.6px] !leading-10 sm:mt-7 md:mt-0"
+        >
+          AI-Powered Search Filtering — switch between smart and precise
+        </h2>
+        <p class="text-grayish-cyan leading-relaxed">
+          Our AI-powered search goes beyond keywords to understand intent, delivering more accurate
+          job matches. Easily switch between AI search and regular filters explores the database semantically, while filters give precise control, all without losing
+          your selected preferences.
+        </p>
+      </div>
+      <div class="order-1 md:order-2 relative sm:h-72 xl:h-[330px] 2xl:h-[450px]">
+        <div
+          class="p-0 lg:h-inherit flex items-center justify-center before:content-[''] before:absolute before:h-full before:sm:h-3/4 before:2xl:h-[372px] before:top-0 before:right-0 before:w-4/5 before:xl:w-[93%] before:rounded-l-lg before:bg-grayish-cyan"
+        >
+          <img
+            src="@/assets/img/ai-powered-search.png"
+            class="relative w-[90%] md:mt-6 xl:mt-9 ml-auto  object-cover"
+            alt="map"
+          />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
