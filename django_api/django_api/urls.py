ebbb504f0e8c85245ab9742b1e693b496a14a5ae
Hikmahx <hikmayousuph@gmail.com>
2026-01-09 22:57:45 +0100

    accounts and companies urls

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/jobs/', include('jobs.api.urls'))
+    path('api/jobs/', include('jobs.api.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/companies/', include('companies.api.urls')),
 ]
