08b5231212e166dbe2b8578816bc9c0d510ed1d3
Hikmahx <hikmayousuph@gmail.com>
2026-01-10 14:28:22 +0100

    account models registered

--- django_api/accounts/admin.py
+++ django_api/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models import User, JobSeekerProfile, FounderProfile
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(JobSeekerProfile)
+admin.site.register(FounderProfile)
