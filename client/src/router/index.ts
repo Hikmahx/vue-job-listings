4ea8834371308a5347ad7c38ea8e9287cc4721be
Hikmahx <hikmayousuph@gmail.com>
2026-01-23 20:25:45 +0100

    router changed

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,12 +1,18 @@
-import { createWebHistory, createRouter } from "vue-router"
+import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from "@/views/HomeView.vue"
 import JobsView from "@/views/JobsView.vue"
 import JobDetailView from "@/views/JobDetailView.vue"
 import LoginView from "@/views/LoginView.vue"
 import SignupView from "@/views/SignupView.vue"
-// import JobApplicationView from "@/views/JobApplicationView.vue"
-// import CreateJobView from "@/views/CreateJobView.vue"
-// import DashboardView from "@/views/DashboardView.vue"
+// import type { RouteRecordRaw } from 'vue-router'
+// import MainLayout from '@/layouts/MainLayout.vue'
+// import DashboardOverviewView from '@/views/DashboardOverview.vue'
+import JobBoardView from '@/views/JobBoard.vue'
+import SettingsView from '@/views/Settings.vue'
+import CreateJobView from '@/views/CreateJob.vue'
+import NotFoundView from '@/views/NotFound.vue'
+import MainLayout from '../components/dashboard/MainLayout.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const routes = [
   { path: "/", component: HomeView },
@@ -17,9 +23,43 @@ const routes = [
   // { path: "/apply/:jobId", component: JobApplicationView, name: "apply" },
   // { path: "/create-job", component: CreateJobView, name: "create-job" },
   // { path: "/dashboard", component: DashboardView, name: "dashboard" },
+  {
+    path: '/',
+    redirect: '/dashboard'
+  },
+  {
+    path: '/dashboard',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Dashboard,
+        name: 'Dashboard'
+      },
+      {
+        path: 'jobs',
+        component: JobBoardView,
+        name: 'JobBoard'
+      },
+      {
+        path: 'settings',
+        component: SettingsView,
+        name: 'Settings'
+      },
+      {
+        path: 'create-job',
+        component: CreateJobView,
+        name: 'CreateJob'
+      }
+    ]
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFoundView
+  }
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
