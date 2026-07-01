3cdb20f9289da845801ed2fa6cb8b248cf789dcc
Hikmahx <hikmayousuph@gmail.com>
2026-07-01 21:58:44 +0100

    fix: Update token refresh view import to use TokenRefreshView from rest_framework_simplejwt

--- django_api/accounts/api/urls.py
+++ django_api/accounts/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from accounts.api.views import RegisterAPI, LoginAPI, LogoutAPI, UserProfileAPI, TokenRefreshAPIView
+from rest_framework_simplejwt.views import TokenRefreshView
+from accounts.api.views import RegisterAPI, LoginAPI, LogoutAPI, UserProfileAPI
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', LogoutAPI.as_view(), name='logout'),
     path('profile/', UserProfileAPI.as_view(), name='profile'),
-    path('token/refresh/', TokenRefreshAPIView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
