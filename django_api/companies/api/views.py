b6706a971b116567ca0cb2a9f0348b04ae5c9d09
Hikmahx <hikmayousuph@gmail.com>
2026-07-03 21:16:30 +0100

    fix: Update permission classes to use IsPrimaryFounder instead of IsCompanyOwner

--- django_api/companies/api/views.py
+++ django_api/companies/api/views.py
@@ -8,7 +8,7 @@ from companies.api.serializers import (
     CompanyDetailSerializer,
     CompanyMemberSerializer,
 )
-from companies.api.permissions import IsCompanyFounder, IsCompanyOwner
+from companies.api.permissions import IsCompanyFounder, IsPrimaryFounder
 from accounts.models import User
 
 
@@ -76,7 +76,7 @@ class DeleteCompanyAPI(generics.DestroyAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
     lookup_field = "slug"
-    permission_classes = [permissions.IsAuthenticated, IsCompanyOwner]
+    permission_classes = [permissions.IsAuthenticated, IsPrimaryFounder]
 
 class AddFounderAPI(APIView):
     permission_classes = [permissions.IsAuthenticated, IsCompanyFounder]
@@ -110,7 +110,7 @@ class AddFounderAPI(APIView):
 
 
 class RemoveFounderAPI(APIView):
-    permission_classes = [permissions.IsAuthenticated, IsCompanyOwner]
+    permission_classes = [permissions.IsAuthenticated, IsPrimaryFounder]
 
     def delete(self, request, slug, member_id):
         company = get_object_or_404(Company, slug=slug)
